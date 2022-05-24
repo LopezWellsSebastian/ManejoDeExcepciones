f813ea419bdd90cc1e4f8f863470c77976de81a2
LopezWellsSebastian <sebastianwelld33@gmail.com>
2022-05-24 11:26:36 -0700

    Programa
    Se agrega un programa

---
+++ ManejoDeExcepciones/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejoDeExcepciones
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int  Num;
+            double NumCuadrado;
+            try
+            {
+                Console.Write("Ingresa un numero entero para elevar al cuadrado: ");
+                Num = int.Parse(Console.ReadLine());
+                NumCuadrado = Math.Pow(Num, 2);
+                
+            }
+            catch(FormatException x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            catch (OverflowException x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            catch(NullReferenceException x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+            }
+        }
+    }
+}
